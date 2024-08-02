996a3f2c1e50f5c35e5aab1e433cb10fe82eac63
Claire Wambani <clairmaks@gmail.com>
2024-08-03 01:14:21 +0300

    db,rest_api update

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,37 @@
-from backend import create_app
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
-app = create_app()
+db = SQLAlchemy()
 
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/animal_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    CORS(app)  # Optional: Enable CORS
+
+    @app.route('/api/animals', methods=['GET'])
+    def get_animals():
+        animals = Animal.query.all()
+        return jsonify([{
+            'id': animal.id,
+            'name': animal.name,
+            'breed': animal.breed,
+            'age': animal.age
+        } for animal in animals])
+
+    @app.route('/api/animals', methods=['POST'])
+    def add_animal():
+        data = request.get_json()
+        new_animal = Animal(
+            name=data['name'],
+            breed=data['breed'],
+            age=data['age']
+        )
+        db.session.add(new_animal)
+        db.session.commit()
+        return jsonify({'id': new_animal.id}), 201
+
+    return app
